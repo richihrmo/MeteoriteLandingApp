d3f326cee72916da63d5525d255326d2784e8ced
Richard Hrmo <xhrmo@fi.muni.cz>
2017-08-19 18:16:59 +0200

    MainActivity cleanup

--- app/src/main/java/com/developers/team100k/meteoritelandingapp/MainActivity.java
+++ app/src/main/java/com/developers/team100k/meteoritelandingapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.developers.team100k.meteoritelandingapp;
 
-import android.app.DownloadManager.Request;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,9 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response.ErrorListener;
@@ -20,28 +17,10 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.developers.team100k.meteoritelandingapp.Adapter.MyAdapter;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import okhttp3.ResponseBody;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import org.json.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -77,13 +56,30 @@ public class MainActivity extends AppCompatActivity {
         startActivity(mapsIntent);
       }
     });
+    JsonFromURL();
+  }
 
+  public void JsonToCollection(String json){
+//    Type type = new TypeToken<List<Meteorite>>(){}.getType();
+//    meteorites = gson.fromJson(json, type);
+    meteorites = getList(Meteorite[].class, json);
+//    Collections.sort(meteorites, new CustomComparator());
+  }
+
+  public static final <T> List<T> getList(final Class<T[]> clazz, final String json)
+  {
+    final T[] jsonToObject = new Gson().fromJson(json, clazz);
+
+    return Arrays.asList(jsonToObject);
+  }
+
+  public void JsonFromURL(){
     RequestQueue queue = Volley.newRequestQueue(this);
     StringRequest stringRequest = new StringRequest(url, new Listener<String>() {
       @Override
       public void onResponse(String response) {
         json = response;
-        Jsonrandom(json);
+        JsonToCollection(json);
         adapter.setList(meteorites);
         mListView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
@@ -95,20 +91,6 @@ public class MainActivity extends AppCompatActivity {
       }
     });
     queue.add(stringRequest);
-  }
-
-  public void Jsonrandom(String json){
-//    Type type = new TypeToken<List<Meteorite>>(){}.getType();
-//    meteorites = gson.fromJson(json, type);
-    meteorites = getList(Meteorite[].class, json);
-    Collections.sort(meteorites, new CustomComparator());
-  }
-
-  public static final <T> List<T> getList(final Class<T[]> clazz, final String json)
-  {
-    final T[] jsonToObject = new Gson().fromJson(json, clazz);
-
-    return Arrays.asList(jsonToObject);
   }
 
   public class CustomComparator implements Comparator<Meteorite> {
